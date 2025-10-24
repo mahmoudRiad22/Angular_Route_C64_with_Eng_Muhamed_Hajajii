5461276febc8bda54082bf048c055607c826b0bf
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-10-24 07:00:28 +0300

    added a summary.notes.ts file

---
+++ projects/week01-app/Summary.Notes.ts
@@ -0,0 +1,27 @@
+/*
+!
+?
+-
+*
+? 1- Angular history:
+      * simply we started with html, css, vanilla-js
+      * in 2010: google produced "angular js" known as "angular-V01"
+      * in 2012: cuz js is "loosely", Microsoft introduced the "typescript" as a way to add data types interface to js.
+      * in 2014: google produced angular-V02 which combined [angular, typescript] the first stable angular version with typescript.
+      * since 2014: google keeps updating angular with v03[never released],v04,v05,v06,v07,v08,v09,v09,v10,v11,v12,v13,v14,v15,v16
+      * in v017: a big change happened where "syntax & bundler" was hugely improved and updated.
+      * after that: for v18,v19,v20..normal improvement and updates except for v20 which will have big change still unknown but will be stable in v21.
+
+? 2- added value:
+    -easier development process:
+        -- DOM injection: you can simply drop any property from TS file into the html file using {{}}.
+        -- Validation: better validation and easier.
+        -- Security: better XSS security and stuff like that.
+        -- Component based architecture.
+        -- SPA.
+
+? 3- framework vs library:
+
+? 4- install angular
+
+*/
