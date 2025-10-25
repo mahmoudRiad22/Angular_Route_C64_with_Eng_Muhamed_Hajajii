1b37b86de2dd5ba6eabbb910f333f77c24bf4430
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-10-25 15:54:20 +0300

    finished the fully applying of images exchange upon click event

--- projects/week02-app/src/app/components/image-handling/image-handling.component.ts
+++ projects/week02-app/src/app/components/image-handling/image-handling.component.ts
@@ -1,5 +1,6 @@
 import { RouterLinkActive, RouterLink } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, provideZoneChangeDetection } from '@angular/core';
+import { TaggedTemplateLiteral } from '@angular/compiler';
 
 @Component({
     selector: 'app-image-handling',
@@ -11,19 +12,30 @@ export class ImageHandlingComponent {
     base = '../../../../public/images/';
     imgUrl = this.base.slice(18);
     images: string[] = [];
-    imagesAlts:string[] = [];
+    imagesAlts: string[] = [];
 
-    ngOnInit():void{
-        this.createImages()
+    ngOnInit(): void {
+        this.createImages();
+    }
+    exchangeImage(e: PointerEvent): void {
+        const target = e.target as HTMLElement;
+
+        if (target && target.attributes[2]) {
+            const temp = target.getAttribute('src');
+            // console.log('temp :>> ', temp);
+            // console.log(target.setAttribute('src', this.images[0]));
+            target.setAttribute('src', this.images[0]);
+            this.images[0] = temp as string;
+        }
     }
     createImages(): void {
         this.images = [];
-        this.imagesAlts =[];
+        this.imagesAlts = [];
         for (let i = 0; i < 6; i++) {
             const randomNum = Math.floor(Math.random() * 61) + 1;
             this.images.push(`${this.imgUrl}${randomNum}.jpg`);
-            this.imagesAlts.push(`${randomNum}.jpg`)
+            this.imagesAlts.push(`${randomNum}.jpg`);
         }
-        console.log('this.images :>> ', this.images);
+        // console.log('this.images :>> ', this.images);
     }
 }
