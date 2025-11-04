97db4dce77c53a8f8dc16bbe4db63d1195ac5229
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-04 13:32:18 +0200

    finished the contact component

--- projects/assignment01-app/src/app/components/contact/contact.component.ts
+++ projects/assignment01-app/src/app/components/contact/contact.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-contact',
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css',
+    selector: 'app-contact',
+    imports: [],
+    templateUrl: './contact.component.html',
+    styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+    userNameActive: boolean = true;
+    userAgeActive: boolean = true;
+    userEmailActive: boolean = true;
+    userPasswordActive: boolean = true;
 
+    showUserNameLabel(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        console.log('input :>> ', input.getAttribute('id'));
+
+        if (input.value && input.getAttribute('id') === 'userName') this.userNameActive = false;
+        else this.userNameActive = true;
+    }
+
+    showUserAgeLabel(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        console.log('input :>> ', input.getAttribute('id'));
+
+        if (input.value && input.getAttribute('id') === 'userAge') this.userAgeActive = false;
+        else this.userAgeActive = true;
+    }
+
+    showUserEmailLabel(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        console.log('input :>> ', input.getAttribute('id'));
+
+        if (input.value && input.getAttribute('id') === 'userEmail') this.userEmailActive = false;
+        else this.userEmailActive = true;
+    }
+
+    showUserPasswordLabel(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        console.log('input :>> ', input.getAttribute('id'));
+
+        if (input.value && input.getAttribute('id') === 'userPassword')
+            this.userPasswordActive = false;
+        else this.userPasswordActive = true;
+    }
 }
