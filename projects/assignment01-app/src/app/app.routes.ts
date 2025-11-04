88527e12fc5ee3d7c5a669882592c0b854bbef5f
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-04 06:45:44 +0200

    first part of the app 1- installed bs and fontawesome 2- created all components (navbar,footer,home,about,portfolio,contact) 3- finished the navbar with routing 4- finished the home page 5- finished the footer

--- projects/assignment01-app/src/app/app.routes.ts
+++ projects/assignment01-app/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProtfolioComponent } from './components/protfolio/protfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:HomeComponent,
+        title:"Home"
+    },
+    {
+        path: 'about',
+        component:AboutComponent,
+        title:"About"
+    },
+    {
+        path: 'portfolio',
+        component:ProtfolioComponent,
+        title: "Portfolio",
+    },
+    {
+        path:"contact",
+        component:ContactComponent,
+        title:'Contact'
+    },
+    {
+        path:"**",
+        component:NotFoundComponent,
+        title:"Not-Found"
+    }
+];
