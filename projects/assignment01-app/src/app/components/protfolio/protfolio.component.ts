9719f84e1c32155adb606ad977ecd831de85253c
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-04 11:14:48 +0200

    finished the portfolio component

--- projects/assignment01-app/src/app/components/protfolio/protfolio.component.ts
+++ projects/assignment01-app/src/app/components/protfolio/protfolio.component.ts
@@ -1,11 +1,27 @@
+import { compileNgModule } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-protfolio',
-  imports: [],
-  templateUrl: './protfolio.component.html',
-  styleUrl: './protfolio.component.css',
+    selector: 'app-protfolio',
+    imports: [],
+    templateUrl: './protfolio.component.html',
+    styleUrl: './protfolio.component.css',
 })
 export class ProtfolioComponent {
+    flag: boolean = true;
+    ClickedImg: string = '';
+    openOverlay(event: PointerEvent): void {
+        const target = event.target as HTMLElement;
+        const imgElement = target.closest('.col-lg-4')?.querySelector('img') as HTMLImageElement;
 
+        if (imgElement?.src) {
+            console.log('Image source:', imgElement.src.slice(21));
+            this.ClickedImg = imgElement.src;
+            this.flag = false;
+        }
+    }
+    closeOverlay(): void {
+        console.log('close this now');
+        this.flag = !this.flag;
+    }
 }
