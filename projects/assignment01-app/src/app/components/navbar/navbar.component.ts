5eab4c6eb00ee2d2968d1384cfffc5554b9cb321
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-05 12:42:37 +0200

    adjusted the prettier tab width to be 4 added a scroll effect to the navbar

--- projects/assignment01-app/src/app/components/navbar/navbar.component.ts
+++ projects/assignment01-app/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from "@angular/router";
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [RouterLink,RouterModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+    selector: 'app-navbar',
+    imports: [RouterLink, RouterModule],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+    navbarPadding = ' py-lg-4';
 
+    @HostListener('document:scroll', ['$event'])
+    changeNavbarPadding(event: Event): void {
+        if (window.scrollY > 120) {
+            this.navbarPadding = ' py-lg-3';
+        } else {
+            this.navbarPadding = ' py-lg-4';
+        }
+    }
 }
