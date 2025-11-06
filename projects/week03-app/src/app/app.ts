da828e6a610436808fd5136e24d7511e8685c2e6
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-06 12:47:05 +0200

    finished the session and its todo lists.. next finish the todo list one by one

--- projects/week03-app/src/app/app.ts
+++ projects/week03-app/src/app/app.ts
@@ -1,14 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [NavbarComponent, FooterComponent, FormsModule],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('week03-app');
+    protected readonly title = signal('week03-app');
+
+    textAreaInput!: string;
+
+    showText() {
+        console.log('textAreaInput :>> ', this.textAreaInput);
+  }
 }
