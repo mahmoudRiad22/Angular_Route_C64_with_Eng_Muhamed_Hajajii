1fafa1f14882c3eefe3e59875fef7dbccdfe5c19
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-06 15:03:54 +0200

    Done: todo 03 and todo 04

--- projects/week03-app/src/app/components/intro/intro.component.ts
+++ projects/week03-app/src/app/components/intro/intro.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-intro',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.css',
 })
 export class IntroComponent {
 
 
-  textAreaInput !:string
+  textAreaInput:string =''
+  userData = {
+    name:'',
+    email:'',
+    password:''
+  }
   isShow:boolean = true
   readInput(event:Event):void{
     this.textAreaInput = (event.target as HTMLInputElement).value
@@ -18,5 +24,9 @@ export class IntroComponent {
 
   toggleShow():void{
     this.isShow = !this.isShow
+  }
+
+  showUserData():void{
+    console.log('userData :>> ', this.userData);
   }
 }
