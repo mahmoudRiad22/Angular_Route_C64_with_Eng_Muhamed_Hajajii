6543b3b3cadf545fa0f7806ad2fc832198eae880
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-08 16:35:10 +0200

    Studying Week 03 Finished :))
    Not-Done: 9 todos

--- projects/week03-app/src/app/app.ts
+++ projects/week03-app/src/app/app.ts
@@ -1,43 +1,42 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { FormsModule, NgModel } from '@angular/forms';
-import { IntroComponent } from './components/intro/intro.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [FormsModule, NavbarComponent, FooterComponent, RouterOutlet],
-    templateUrl: './app.html',
-    styleUrl: './app.css',
+  selector: 'app-root',
+  imports: [FormsModule, NavbarComponent, FooterComponent, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
 })
 export class App {
-    protected readonly title = signal('week03-app');
+  protected readonly title = signal('week03-app');
 
-    textAreaInput!: string;
+  textAreaInput!: string;
 
-    showText() {
-        console.log('textAreaInput :>> ', this.textAreaInput);
-    }
+  showText() {
+    console.log('textAreaInput :>> ', this.textAreaInput);
+  }
 }
 
 // was working on understanding OOP in TypeScript
 abstract class name {
-    constructor(parameters: string) {
-        this.firstName = parameters;
-    }
-    lastName: string = 'Riad';
-    firstName: string;
+  constructor(parameters: string) {
+    this.firstName = parameters;
+  }
+  lastName: string = 'Riad';
+  firstName: string;
 
-    abstract sayHello(): string;
+  abstract sayHello(): string;
 }
 
 class myName extends name {
-    sayHello(): string {
-        return 'hello';
-    }
+  sayHello(): string {
+    return 'hello';
+  }
 }
 
 const me = new myName('mahmoud');
 
-console.log(me.firstName, me.lastName,'says', me.sayHello());
+console.log(me.firstName, me.lastName, 'says', me.sayHello());
