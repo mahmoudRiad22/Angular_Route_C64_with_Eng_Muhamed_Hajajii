42aac7220af919b1bae6b8c4874f27380cf515ee
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-08 17:46:13 +0200

    created components

---
+++ projects/week04-app/src/app/components/about/components/about-body/about-body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-body',
+  imports: [],
+  templateUrl: './about-body.component.html',
+  styleUrl: './about-body.component.css',
+})
+export class AboutBodyComponent {
+
+}
---
+++ projects/week04-app/src/app/components/about/components/about-footer/about-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-footer',
+  imports: [],
+  templateUrl: './about-footer.component.html',
+  styleUrl: './about-footer.component.css',
+})
+export class AboutFooterComponent {
+
+}
