6e6b4fa3269b428249b64a150315eff3a5bd8dc1
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-09 15:44:08 +0200

    created and routed userProfile component

--- projects/week04-app/src/app/app.routes.ts
+++ projects/week04-app/src/app/app.routes.ts
@@ -3,37 +3,41 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 export const routes: Routes = [
-  
   {
-    path:'',
-    redirectTo:'home',
-    pathMatch:"full"
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
   {
-    path:'home',
-    component:HomeComponent,
-    title: "Week04-app - Home"
+    path: 'home',
+    component: HomeComponent,
+    title: 'Week04-app - Home',
   },
   {
-    path:'about',
-    component:AboutComponent,
-    title: "Week04-app - about"
+    path: 'about',
+    component: AboutComponent,
+    title: 'Week04-app - about',
   },
   {
-    path:'contact',
-    component:ContactComponent,
-    title: "Week04-app - contact"
+    path: 'contact',
+    component: ContactComponent,
+    title: 'Week04-app - contact',
   },
   {
-    path:'not-found',
-    component:NotFoundComponent,
-    title:'Not-Found 404'
+    path: 'profile',
+    component: UserProfileComponent,
+    title: 'Week04-app - Profile',
   },
   {
-    path:'**',
-    redirectTo:'not-found'
+    path: 'not-found',
+    component: NotFoundComponent,
+    title: 'Not-Found 404',
   },
-
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  },
 ];
