3b1420aee88c68b87302feea594911e49dcefd2c
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-14 18:31:58 +0200

    1- updated README file with proper configuration for tailwind 2- fixed a double quoted path in content array, inside tailwindcss.config.js

--- projects/tailwind3-app/tailwind.config.js
+++ projects/tailwind3-app/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",   // important: include .ts because Angular templates often live in .ts or .html
+    './src/**/*.{html,ts}', // important: include .ts because Angular templates often live in .ts or .html
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
