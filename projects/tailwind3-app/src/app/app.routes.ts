c0a10912d4c73425736f3b31d9ca62c5a42bc38b
mahmoudRiad22 <mahmoudriad027@gmail.com>
2025-11-15 16:33:40 +0200

    1- added not-found component and its proper routing 2- created main bar and its responsiveness
    3- practicing navbar multiple times

--- projects/tailwind3-app/src/app/app.routes.ts
+++ projects/tailwind3-app/src/app/app.routes.ts
@@ -2,11 +2,29 @@ import { Routes } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 
 export const routes: Routes = [
-  
   {
-    path: 'core-concept',
+    path: '',
+    redirectTo: 'coreConcepts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'coreConcepts',
     loadComponent: () =>
       import('./components/core-concepts/core-concepts').then((m) => m.CoreConcepts),
     title: 'Tailwind3 - Core Concept',
   },
+  {
+    path: 'not-found',
+    loadComponent: () => {
+      return import('./components/not-found/not-found').then((m) => {
+        return m.NotFound;
+      });
+    },
+    title: 'Tailwind3 - 404-Page Not Found!',
+  },
+  {
+    path:"**",
+    redirectTo:"not-found",
+
+  }
 ];
--- projects/tailwind3-app/src/app/components/navbar/navbar.ts
+++ projects/tailwind3-app/src/app/components/navbar/navbar.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, ElementRef, NgModule, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
 
+
+  toggle = true
+@ViewChild('navLinks') navLinks!:ElementRef
+showNavLinks():void{
+  // if ((this.navLinks.nativeElement as HTMLElement).classList.contains('hidden'))
+  //     (this.navLinks.nativeElement as HTMLElement).classList.remove('hidden')
+  console.log(this.toggle);
+  this.toggle = !this.toggle
+}
 }
